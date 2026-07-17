6482df4d124875b1c44c1f762a4193c2c33275d8
TinhVan2809 <tinhlu703@gmail.com>
2026-07-17 14:13:36 +0700

    feat: add password change modal to PasswordSetting component

--- frontend-native/app/(setting)/password-setting.tsx
+++ frontend-native/app/(setting)/password-setting.tsx
@@ -1,25 +1,62 @@
 import { ScreenShell } from "@/components/screen-shell";
 import { Ionicons } from "@expo/vector-icons";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Modal, TextInput } from "react-native";
 import { useRouter } from "expo-router";
+import { useState } from "react";
 export default function PasswordSetting() {
   const router = useRouter();
+
+  const [isPasswordModal, setIsPasswordModal] = useState(false);
+
+  const onPassowrdModal = () => {
+    setIsPasswordModal(true);
+  }
+
   return (
-    <ScreenShell
-      left={
-        <View style={styles.title}>
-          <Pressable onPress={() => router.back()}>
-            <Ionicons name="arrow-back-outline" size={22} />
+    <>
+      <ScreenShell
+        left={
+          <View style={styles.title}>
+            <Pressable onPress={() => router.back()}>
+              <Ionicons name="arrow-back-outline" size={22} />
+            </Pressable>
+            <Text>Mật khẩu và bảo mật</Text>
+          </View>
+        }
+      >
+        <View>
+          <Pressable onPress={onPassowrdModal}>
+            <Text>Đổi mật khẩu</Text>
           </Pressable>
-          <Text>Mật khẩu và bảo mật</Text>
+          <Pressable>
+            <Text>Xác thực 2 yếu tố</Text>
+          </Pressable>
         </View>
-      }
-    >
-      <View>
-        <Text>Đổi mật khẩu</Text>
-        <Text>Xác thực 2 yếu tố</Text>
-      </View>
-    </ScreenShell>
+      </ScreenShell>
+
+      {isPasswordModal && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+        >
+          <View>
+            <TextInput placeholder="Nhập mật khẩu hiện tại" />
+          </View>
+          <View>
+            <TextInput placeholder="Nhập mật khẩu mới" />
+          </View>
+          <View>
+            <TextInput placeholder="Xác nhận mật khẩu mới" />
+          </View>
+
+          <View>
+            <Text>
+              Thay đổi
+            </Text>
+          </View>
+        </Modal>
+      )}
+    </>
   );
 }
 
